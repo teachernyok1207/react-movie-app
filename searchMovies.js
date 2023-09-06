4e2622eb1250a1838c69259b1f1bb8d0b3272b58
Norman Galanza <104883084+teachernyok1207@users.noreply.github.com>
2023-09-07 00:12:44 +0800

    Add files via upload

---
+++ searchMovies.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+
+export default function SearchMovies(){
+    
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
+    
+    const searchMovies = (e) => {
+        e.preventDefault();
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => setMovies(data.results))
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
+    return (
+        <div>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query"
+                    placeholder="i.e. Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img className="card--image"
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={movie.title + ' poster'}
+                            />
+                        <div className="card--content">
+                        <h3 className="card--title">{movie.title}</h3>
+                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                        <p><small><i className="fa-solid fa-star rate-star"></i>: ({movie.vote_average.toFixed(2)})</small></p>
+                        <p className="card--desc">{movie.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>    
+        </div>
+    )
+}
